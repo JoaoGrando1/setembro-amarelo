436389716dce52c02c25c63f8b9b8f2b0b6fec8d
JoaoGrando1 <134527296+JoaoGrando1@users.noreply.github.com>
2024-10-02 15:40:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
+
+const backToTopButton = document.getElementById("backToTop");
+
+// Mostra ou esconde o botão ao rolar
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = "block"; // Mostra o botão
+    } else {
+        backToTopButton.style.display = "none"; // Esconde o botão
+    }
+};
+
+// Define a ação do botão
+backToTopButton.onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Rolagem suave
+    });
+};
